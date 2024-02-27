99dcc2b7cf485f96d9b85ff87b1249be72c54fe8
bhanu <prasad.bhanu59@gmail.com>
2024-02-27 12:53:08 +0530

    added azure storage account

--- Azure_srotage_account/main.tf
+++ Azure_srotage_account/main.tf
@@ -2,20 +2,20 @@ resource "azurerm_resource_group" "my_res_grp" {
     name            = "fromterraform"
     location        = "eastus"
     tags            =  {
-        project     = "qtlearning",
+        project     = "learning",
         env         = "dev"
     } 
 
 }
 
 resource "azurerm_storage_account" "my_store_acc" {
-    name                        = "qtmystorageacctfoct14"
+    name                        = "mystorageacctfoct14"
     resource_group_name         = "fromterraform"
     location                    = "eastus" 
     account_tier                = "Standard" 
     account_replication_type    = "GRS"
     tags                        =  {
-        project                 = "qtlearning",
+        project                 = "learning",
         env                     = "dev"
     } 
 
